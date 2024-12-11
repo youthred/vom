a3e6ac018c9e2768f74b44773ddf19ad2e309026
youthred <183159630@qq.com>
2024-12-11 22:08:06 +0800

    toolbar & drawer

--- src/main/java/io/github/youthred/vom/web/router/Router.java
+++ src/main/java/io/github/youthred/vom/web/router/Router.java
@@ -1,14 +1,14 @@
-package io.github.youthred.vom.web.route;
+package io.github.youthred.vom.web.router;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexModel {
+public class Router {
 
-    @GetMapping
+    @GetMapping("/")
     public ModelAndView index() {
-        return new ModelAndView("index", "a", "123123");
+        return new ModelAndView("index.html");
     }
 }
